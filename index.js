087f7b33ab9f60cadda5c2767a563b240bc9c06c
saurabhsensai <saurabhnale9@gmail.com>
2024-11-21 21:04:20 +0530

    ADDED EXTRA DATA

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json")
-
+const fs = require("fs")
 
 const app = express();
 const PORT = 8000
+
+
+app.use(express.urlencoded({extended:false}));
 
 
 app.get('/users' , (req, res) => {
@@ -26,6 +29,35 @@ app.get('/api/users/:id', (req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id );
     return res.json(user);
+})
+
+app.post("/api/users" , (req, res) => {
+    //todo: Create a new user
+    const body = req.body;
+    users.push({id: users.length + 1, ...body});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err , data) => {
+        return res.json({status: "Added"});
+    })
+
+
+    
+    
+
+   
+
+})
+
+app.patch("/api/users/:id" , (res, req) => {
+    //todo: Edit a user with id
+    return req.json({status: "Pending"});
+    
+})
+
+
+app.delete("/api/users/:id" , (res, req) => {
+    //todo: Delete a user with id
+    return req.json({status: "Pending"});
+    
 })
 
 
